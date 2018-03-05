3d850b9c26a51090f7baf9785d21b1e46af7b6d6
lefjor <lefjor@gmail.com>
2018-03-05 22:29:40 +0100

    Initial commit

---
+++ src/main/resources/db/migration/V1__init.sql
@@ -0,0 +1,7 @@
+CREATE TABLE PERSON (
+	id varchar(20) not null,
+	first_name varchar(255) not null,
+	last_name varchar(255) not null
+);
+
+insert into PERSON (id, first_name, last_name) values ('test','Dave', 'Syer');
---
+++ src/main/resources/db/migration/V2__init2.sql
@@ -0,0 +1,1 @@
+insert into PERSON (id, first_name, last_name) values ('test2','Dave2', 'Syer4');
